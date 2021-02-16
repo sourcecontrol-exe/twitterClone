85bceef0cc6b02079d5de802052f65cd63e93eb8
Swetabh Subham <swetabh.subham@gmail.com>
2021-02-17 05:15:10 +0530

    client conect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import  {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
 
+ const client = new ApolloClient({
+   uri: "http://localhost:4000",
+   cache: new InMemoryCache(),
+ })
 function App() {
   return (
+    <ApolloProvider client ={client}>
   <div>
     Hello this is started
   </div> 
+  </ApolloProvider>
   );
 }
 
